791bb2a8d73bd3d1d90d2abb30fea4749d4f5939
302A-19\cre <a>
2024-10-15 14:43:58 +0900

    todo-app 終わり

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -13,7 +13,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-        $tasks = Task::all();
+        $tasks = Task::where('status', false)->get();
         
         return view('tasks.index', compact('tasks'));
     }
@@ -77,7 +77,40 @@ class TaskController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+
+        //「編集する」ボタンをおしたとき
+        if ($request->status === null) {
+        $rules = [
+            'task_name' => 'required|max:100',
+            ];
+            $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
+
+            Validator::make($request->all(), $rules, $messages)->validate();
+
+
+            //該当のタスクを検索
+            $task = Task::find($id);
+
+            //モデル->カラム名 = 値 で、データを割り当てる
+            $task->name = $request->input('task_name');
+
+            //データベースに保存
+            $task->save();
+        } else {
+            //「完了」ボタンを押したとき
+
+            //該当のタスクを検索
+            $task = Task::find($id);
+
+            //モデル->カラム名 = 値 で、データを割り当てる
+            $task->status = true; //true:完了、false:未完了
+
+            //データベースに保存
+            $task->save();
+        }
+
+            //リダイレクト
+            return redirect('/tasks');
     }
 
     /**
@@ -85,6 +118,8 @@ class TaskController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+      Task::find($id)->delete();
+
+      return redirect('/tasks');
     }
 }
